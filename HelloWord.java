016e0693e476ee34416c3983b6d424cd7bf1f70f
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2021-12-30 19:41:06 +0530

    #HelloWorld program
    My first program in jave 
    Thank for To read my program

---
+++ HelloWord.java
@@ -0,0 +1,15 @@
+/**
+ * HelloWord
+ */
+//Enter the java world
+//Your name is shubham 
+//Type frist code in java
+public class HelloWord {//HolloWord is the file name and class name also
+
+    public static void main(String[] args) {//It is the main methad/(funtion)in java
+        System.out.println("Hello Word");//This is the printing output line
+        
+    }//Close the curly bracket for main
+}//Close the curly bracket for class
+
+//**Thanks for to read my frist program **
