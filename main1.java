8cfb90210c453f43dc29fd16cf5ed37a9c2cbd70
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2021-12-31 23:55:42 +0530

    #1sum program

---
+++ main1.java
@@ -0,0 +1,11 @@
+public class main1 {
+    public static void main(String[] args) {
+        System.out.println("the sum of threse number is :");//Print statment in the program
+        int num1=6;//Input int variable
+        int num2=5;//Input int variable
+        int num3=7;//Input int variable
+        int sum = num1 + num2 + num3;//Defind the int variable in the program
+        System.out.println(sum);//secound print statment in sum variable
+
+    }
+}
