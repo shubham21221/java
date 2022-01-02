29627fc547dd95be848c1e24da6964c90b158272
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-02 19:32:22 +0530

    #3String program

---
+++ main2.java
@@ -0,0 +1,18 @@
+public class main2 {
+    public static void main(String[] args) {
+        byte age = 34;
+        int age2 = 56;
+        short age3 = 387;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d = 4.66;
+        boolean a = true;
+        System.out.println(age);
+        String s = "Shubham";//print the string 
+        System.out.println(s);
+
+    }
+
+}
+
+//output == Shubham
