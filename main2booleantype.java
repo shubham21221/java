c327b8316796f0c3a21a7f058e20ee0572648be3
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-03 10:57:25 +0530

    #3boolentype

---
+++ main2booleantype.java
@@ -0,0 +1,8 @@
+public class main2booleantype {
+    
+    public static void main(String[] args) {
+        boolean flag = true;//boolean use in true and false
+        System.out.println(flag);
+    }
+}
+//output is true becouse boolen name is flag
