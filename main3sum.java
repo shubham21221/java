e760e922f6f6d3f8301a8aa7129ff9eaaabe077f
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-04 13:33:33 +0530

    #Sum Program

---
+++ main3sum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;//Input header file
+
+public class main3sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a frist number :");//Enter the frist number
+        int a =sc.nextInt();//Enter the Integer
+        System.out.println("Enter a secound number :");//Enter the secound number
+        int b =sc.nextInt();//Enter the Integer
+        int sum = a+b;//include sum=a+b;
+        System.out.println("the sum is :"+sum);//print the sum Integers
+    }
+}
