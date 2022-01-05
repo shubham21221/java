0cf3da8bfe992c862cdd28b2a25f88addfaa4728
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-05 15:27:43 +0530

    #Boolean program

---
+++ main4boolean.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+//Boolen input program 
+public class main4 {
+    public static void main(String[] args){
+        System.out.println("taking Input of the user:");
+        Scanner sc = new Scanner(System.in);
+        boolean b1= sc.hasNextInt();
+        System.out.println(b1);
+    }
+}
