3dad850cdb09b388efb9cfc8bc098647ca66b668
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-07 18:07:42 +0530

    #Input String

---
+++ main5Inputstring.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+//input the string int the program
+public class main5 {
+    public static void main(String[] args) {
+        System.out.println("enter a string :");
+        Scanner sc =new Scanner(System.in);
+        String s =sc.nextLine();
+        System.out.println(s);
+    }
+}
