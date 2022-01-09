3e5eaca0b3860c6d42ac68cd6f49057e49c4db32
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-09 12:23:06 +0530

    #operator >< True or False

---
+++ main7operator.java
@@ -0,0 +1,11 @@
+public class main7operator {
+    public static void main(String[] args) {
+        int a=4;
+        int b = 9;
+        b*=3;
+        System.out.println(b);//print value int program
+        System.out.println(64<6);//64 < 6 True or false
+    }
+}
+//Output = 27 
+//         false
