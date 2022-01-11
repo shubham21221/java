476592e934c91e066078de8d0d99871849a8b3c8
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-11 10:57:24 +0530

    #calculate the operator

---
+++ main9operator.java
@@ -0,0 +1,11 @@
+public class main9operator {
+    public static void main(String[] args) {
+        int a = 6;
+        int b = 1;
+        int c = 5;
+        int sum = b * b - 4 * a * c / 2 * a;
+        System.out.println(sum);
+
+    }
+}
+//output = -359
