787cd2238ba016e485dd7d2cf7cf61d90da9d6aa
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-12 19:33:57 +0530

    #Increment

---
+++ mainn10increment.java
@@ -0,0 +1,13 @@
+public class mainn10increment {
+    public static void main(String[] args) {
+        int i =45;
+        System.out.println(i++);//print i value
+        System.out.println(i);//increment
+        System.out.println(++i);//print i value
+        System.out.println(i);//increment
+    }
+}
+                        //output = 45
+                        //         46
+                        //         47
+                        //         47
