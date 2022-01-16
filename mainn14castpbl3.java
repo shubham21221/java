6730c4d019f6272b2c7131920d2646a1058ab418
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-16 23:12:11 +0530

    #14cast problem

---
+++ mainn14castpbl3.java
@@ -0,0 +1,14 @@
+public class mainn14castpbl3 {
+    public static void main(String[] args) {
+        char gread ='B';
+        gread =(char)(gread+8);
+        System.out.println(gread);
+        //The gread is the + 8 is char
+
+        gread =(char)(gread-8);
+        System.out.println(gread);
+        //The gread is the - 8 is char
+    }
+}
+//output= J
+//        B
