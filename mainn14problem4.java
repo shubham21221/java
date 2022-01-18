b0fbdf0caa22d8768b4f4cfe3ddc1995d00f1641
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-18 23:14:41 +0530

    #14problem4 less than and getter than operater

---
+++ mainn14problem4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class mainn14problem4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(a>8);
+    }
+}
+//   4
+//   false
+// secound time rune
+//   12
+//   true
