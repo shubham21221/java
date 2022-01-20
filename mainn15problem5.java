afe601ad0d5e5512740a70392d02da27243413b3
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-20 23:54:32 +0530

    #15calculate program

---
+++ mainn15problem5.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+
+public class mainn15problem5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println(7*49/7+35/7);
+    }
+}
