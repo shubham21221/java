e71d6fd2e68da385ffac88831c09bff3498ca91a
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-21 19:41:16 +0530

    #17 Print the int and Float

---
+++ mainn17string.java
@@ -0,0 +1,13 @@
+
+public class mainn17string {
+    public static void main(String[] args) {
+        int a = 4;
+        Float f = 5.89877f;
+        System.out.printf("the value of a %d and value of b is %g",a,f);
+        System.out.println(" ");
+        System.out.format("the value of a %d and value of b is %g",a,f);
+    }
+}
+// output
+//the value of a 4 and value of b is 5.89877 
+// the value of a 4 and value of b is 5.8987
