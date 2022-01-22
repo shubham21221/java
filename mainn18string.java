c8b76a9badde051c70338a5a3c8fe038e2510af8
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-22 22:21:21 +0530

    #18string program

---
+++ mainn18string.java
@@ -0,0 +1,9 @@
+public class mainn18string {
+    public static void main(String[] args) {
+        String name ="shubham";
+        int value = name.length();
+        System.out.println(value);
+    }
+}
+// output
+// 7
