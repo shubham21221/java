677b3284a165850645beeb42e9579fb9bf82f37f
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-22 22:23:45 +0530

    #19 String Lowercase

---
+++ mainn19stringlowercase.java
@@ -0,0 +1,13 @@
+public class mainn19stringlowercase {
+    public static void main(String[] args) {
+        String name ="Shubham";
+        int value = name.length();
+        System.out.println(value);
+
+        String lString = name.toLowerCase();
+        System.out.println(lString);
+    }
+}
+//  output
+//     7
+//    shubham
