0a82ca3ae9c5da327af4d0a8d9a7a46a85d7644a
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-24 23:13:36 +0530

    #20string upper case

---
+++ mainn20stringuppercase.java
@@ -0,0 +1,13 @@
+public class mainn20stringuppercase {
+    public static void main(String[] args) {
+        String name ="Shubham";
+        int value = name.length();
+        System.out.println(value);
+
+        String uString = name.toUpperCase();
+        System.out.println(uString);
+    }
+}
+// output
+//   7
+// SHUBHAM
