a0240789ab9a88ac1f5c0f9bfe7bcd0359675042
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-24 23:14:16 +0530

    #22string sub string

---
+++ mainn22stringsubstring.java
@@ -0,0 +1,10 @@
+public class mainn22stringsubstring {
+    public static void main(String[] args) {
+        String name ="Shubham";
+        System.out.println(name.substring(1));
+        System.out.println(name.substring(1,5));
+    }
+}
+// output
+// hubham
+// hubh
