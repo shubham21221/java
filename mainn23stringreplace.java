79398bd5c1da8bd4bc2fb05223b038a51d0b2051
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-25 23:38:21 +0530

    #23 string replace

---
+++ mainn23stringreplace.java
@@ -0,0 +1,10 @@
+public class mainn23stringreplace {
+    
+    public static void main(String[] args) {
+        String name ="Shubham";
+        System.out.println(name.replace("s","S"));
+        System.out.println(name.replace("shubham","shubhum"));
+    }
+}
+// Shubham
+// Shubham
