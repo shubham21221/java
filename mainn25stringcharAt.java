11a9add256f700723a583ee46044306f3a229887
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-30 11:32:09 +0530

    #25stringcharAt

---
+++ mainn25stringcharAt.java
@@ -0,0 +1,9 @@
+public class mainn25stringcharAt {
+    
+        public static void main(String[] args) {
+            String name ="Shubham";
+            System.out.println(name.charAt(5));
+    }
+}
+// output
+// a
