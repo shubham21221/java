44e648edf917df37716b58ac5cfa950ebddb5e12
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-01-30 11:31:40 +0530

    #24stringstartwith

---
+++ mainn24stringstartwith.java
@@ -0,0 +1,11 @@
+public class mainn24stringstartwith {
+        
+    public static void main(String[] args) {
+        String name ="Shubham";
+        System.out.println(name.startsWith("Shu"));
+        System.out.println(name.startsWith("shu"));
+    }
+}
+// output
+// true 
+// false
