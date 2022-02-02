5c4495bbe721b0f0943b1f63a3a889389a2bfe9b
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-02-02 19:32:09 +0530

    #27stringlastindex

---
+++ mainn27stringlastindex.java
@@ -0,0 +1,8 @@
+public class mainn27stringlastindex {
+    public static void main(String[] args) {
+        String name = "Shubham";
+        System.out.println(name.lastIndexOf("bha"));
+
+    }
+
+}
