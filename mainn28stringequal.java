417276010bb0b0b00acfb2f2c6cf864f28520e4c
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-02-14 21:41:03 +0530

    #28stringequal

---
+++ mainn28stringequal.java
@@ -0,0 +1,8 @@
+public class mainn28stringequal {
+    public static void main(String[] args) {
+        String name = "Shubham";
+        System.out.println(name.equals("Shubham"));// true
+        System.out.println(name.equals("bham"));// false
+
+    }
+}
