2d6c12f619936a6ed964745e34a2109f2597dece
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-02-15 21:55:44 +0530

    #29stringequalIgnorecase

---
+++ mainn29stringequalIgnorecase.java
@@ -0,0 +1,7 @@
+public class mainn29stringequalIgnorecase {
+    public static void main(String[] args) {
+        String name = "Shubham";
+        System.out.println(name.equalsIgnoreCase("shubham"));
+    }
+
+}
