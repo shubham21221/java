dfacc2cf7dc981c43064af0ffea021580b71384f
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-02-16 15:56:42 +0530

    #30ifelsepbl

---
+++ mainn30ifelsepbl1.java
@@ -0,0 +1,10 @@
+public class mainn30ifelsepbl1 {
+    
+    public static void main(String[] args) {
+        int a=10;
+        if(a==11)
+        System.out.println("Equal than of the intager");
+        else
+        System.out.println("Equal tahn any number");
+    }
+}
