04ec44a03708690df85626d40d8173da2d532902
shubham_11 <80152607+shubham21221@users.noreply.github.com>
2022-02-17 22:53:34 +0530

    #8operator

--- main8operator.java
+++ main8operator.java
@@ -2,9 +2,9 @@ public class main8operator {
     public static void main(String[] args) {
         int a=4;
         int b=9;
-        b*=3;System.out.println(64>5 && 64>98);
-        System.out.println(64>5 || 64>98);
+        b*=3;System.out.println(64=>5 && 64=>98);
+        System.out.println(64=>5 || 64=>98);
     }
 }
-//output= false
-//        true
+//output = false
+//         true
